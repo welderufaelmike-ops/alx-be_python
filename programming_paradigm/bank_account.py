79d7460f2e8651e1ab71a407057d28fc1a1bb2af
mike <welderufaelmike@gmail.com>
2025-10-02 12:31:51 +0300

    pp

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+
+class BankAccount:
+    def __init__(self,initial_balance=0.0):
+        self.account_balance=float(initial_balance)
+    
+    def deposit(self,amount):
+        if amount >0:
+            self.account_balance+=amount
+            return True
+        return False
+    def withdraw(self, amount):
+        if 0< amount <= self.account_balance:
+            return True
+        return False
+    
+    def display (self):
+        print(f"the current balance: ${self.account_balance=:.2f}")
